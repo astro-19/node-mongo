d94e3cf1dc06ed64be49ed9f7c7bdbcbab081f74
astro-19 <pushkartas2418@gmail.com>
2022-08-12 22:23:46 +0530

    Node Mongodb example

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dboper = require('./operations');
 
 const url = 'mongodb://localhost:27017'
 
@@ -11,25 +12,51 @@ MongoClient.connect(url, (err, client) => {
 
     const db = client.db(dbname);
 
-    const collection = db.collection('dishes');
+    dboper.insertDocument(db, { name: "Vadonut", description: "Test" }, 'dishes', (result) => {
+        console.log('Insert document: \n', result);
 
-    collection.insertMany([{"name": "Uthappizza", "description": "Test"}, {"name": "Poha", "desciption": "Test one"}], (err, result) => {
-        assert.equal(err, null);
+        dboper.findDocument(db, 'dishes', (docs) => {
+            console.log("Found documents: \n", docs);
 
-        console.log('After insert: \n');
-        console.log(result);
+            dboper.updateDocument(db, { name: "Vadonut"} , { description: "Updated test" }, 'dishes', (result) => {
+                console.log("Updated document: \n", result);
 
-        collection.find({}).toArray((err, docs) => {
-            assert.equal(err, null);
+                dboper.findDocument(db, 'dishes', (docs) => {
+                    console.log("Found documents: \n", docs);
 
-            console.log('Found: \n');
-            console.log(docs);
+                    db.dropCollection('dishes', (result) => {
+                        console.log("Dropped collection: \n", result);
 
-            db.dropCollection('dishes', (err, result) => {
-                assert.equal(err, null);
-
-                client.close();
-            })
+                        client.close();
+                    })
+                });
+            });
         });
     });
-})
+
+});
+
+
+
+
+// const collection = db.collection('dishes');
+
+    // collection.insertMany([{"name": "Uthappizza", "description": "Test"}, {"name": "Poha", "desciption": "Test one"}], (err, result) => {
+    //     assert.equal(err, null);
+
+    //     console.log('After insert: \n');
+    //     console.log(result);
+
+    //     collection.find({}).toArray((err, docs) => {
+    //         assert.equal(err, null);
+
+    //         console.log('Found: \n');
+    //         console.log(docs);
+
+    //         db.dropCollection('dishes', (err, result) => {
+    //             assert.equal(err, null);
+
+    //             client.close();
+    //         })
+    //     });
+    // });
